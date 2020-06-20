fc74bbb4c9b84207398e110082ced7ac6ffcc7bc
Kenski74 <67193394+Kenski74@users.noreply.github.com>
2020-06-20 15:55:20 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,31 @@
+let cas1 = 0
+let hra_zacala = false
+let start_cas = 0
+let reakcia1 = 0
+let reakcia2 = 0
+let cas2 = 0
 input.onPinPressed(TouchPin.P0, function () {
     cas1 = input.runningTime()
-    reakcia1 = cas1 - start_cas
 })
 input.onPinPressed(TouchPin.P2, function () {
-    reakcia1 = 0
-    reakcia2 = 0
     for (let _index = 0; _index <= 2; _index++) {
         basic.showNumber(3 - _index)
         basic.pause(100)
         basic.clearScreen()
     }
-    oneskorenie = 200 * randint(3, 9)
-    basic.pause(oneskorenie)
+    basic.pause(randint(0, 5000))
     basic.showIcon(IconNames.Skull)
     hra_zacala = true
     start_cas = input.runningTime()
+    reakcia1 = 0
+    reakcia2 = 0
 })
 input.onPinPressed(TouchPin.P1, function () {
     cas2 = input.runningTime()
-    reakcia2 = cas2 - start_cas
 })
-let cas2 = 0
-let oneskorenie = 0
-let reakcia2 = 0
-let start_cas = 0
-let reakcia1 = 0
-let cas1 = 0
-let hra_zacala = false
-hra_zacala = false
 basic.forever(function () {
-    while (hra_zacala && (reakcia1 > 0 || reakcia2 > 0)) {
-        if (reakcia1 > reakcia2) {
+    while (hra_zacala) {
+        if (cas1 < cas2 && cas1 > start_cas || cas2 < start_cas) {
             basic.showLeds(`
                 . . # . .
                 . # . . .
@@ -38,21 +33,14 @@ basic.forever(function () {
                 . # . . .
                 . . # . .
                 `)
-        } else if (reakcia1 < reakcia2) {
+        }
+        if (cas2 < cas1 && cas2 > start_cas || cas1 < start_cas) {
             basic.showLeds(`
                 . . # . .
                 . . . # .
                 # # # # #
                 . . . # .
                 . . # . .
-                `)
-        } else {
-            basic.showLeds(`
-                # . . . #
-                . # . # .
-                . . # . .
-                . # . # .
-                # . . . #
                 `)
         }
         hra_zacala = false
