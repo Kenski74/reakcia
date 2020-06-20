261121b7fd96a77a8d9757829e5ca2a48c21a79d
Kenski74 <67193394+Kenski74@users.noreply.github.com>
2020-06-20 17:00:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,29 @@
 let cas1 = 0
+let cas2 = 0
 let hra_zacala = false
 let start_cas = 0
-let reakcia1 = 0
-let reakcia2 = 0
-let cas2 = 0
 input.onPinPressed(TouchPin.P0, function () {
     cas1 = input.runningTime()
 })
 input.onPinPressed(TouchPin.P2, function () {
+    cas1 = 1e+21
+    cas2 = 1e+21
     for (let _index = 0; _index <= 2; _index++) {
         basic.showNumber(3 - _index)
         basic.pause(100)
         basic.clearScreen()
     }
-    basic.pause(randint(0, 5000))
+    basic.pause(randint(0, 3000))
     basic.showIcon(IconNames.Skull)
     hra_zacala = true
     start_cas = input.runningTime()
-    reakcia1 = 0
-    reakcia2 = 0
 })
 input.onPinPressed(TouchPin.P1, function () {
     cas2 = input.runningTime()
 })
 basic.forever(function () {
     while (hra_zacala) {
-        if (cas1 < cas2 && cas1 > start_cas || cas2 < start_cas) {
+        if (cas1 > start_cas && cas1 < cas2 || cas2 < start_cas) {
             basic.showLeds(`
                 . . # . .
                 . # . . .
@@ -33,8 +31,11 @@ basic.forever(function () {
                 . # . . .
                 . . # . .
                 `)
+            cas1 = 1e+21
+            cas2 = 1e+21
+            hra_zacala = false
         }
-        if (cas2 < cas1 && cas2 > start_cas || cas1 < start_cas) {
+        if (cas2 > start_cas && cas2 < cas1 || cas1 < start_cas) {
             basic.showLeds(`
                 . . # . .
                 . . . # .
@@ -42,7 +43,9 @@ basic.forever(function () {
                 . . . # .
                 . . # . .
                 `)
+            cas1 = 1e+21
+            cas2 = 1e+21
+            hra_zacala = false
         }
-        hra_zacala = false
     }
 })
